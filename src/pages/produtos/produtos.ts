f81dbe14cdfb5550a0ceb10d8f37e035ff5ec039
Alisson Oliveira <alissonoliveira.dev@gmail.com>
2020-11-09 18:47:28 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -22,6 +22,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -33,29 +37,35 @@ export class ProdutosPage {
       error => {
         loader.dismiss();
       });
-    }
+  }
 
-    loadImageUrls() {
-      for (var i=0; i<this.items.length; i++) {
-        let item = this.items[i];
-        this.produtoService.getSmallImageFromBucket(item.id)
-          .subscribe(response => {
-            item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
-          },
-          error => {});
-      }
-    } 
-    
-    showDetail(produto_id : string) {
-      this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
     }
-    
-    presentLoading() {
-      let loader = this.loadingCtrl.create({
-        content: "Aguarde..."
-      });
-      loader.present();
-      return loader;
-    }
+  }  
+
+  showDetail(produto_id : string) {
+    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);    
+  }
 }
-
